83f011a3b10c6984244b956a7516dcd14e5aaf83
Andre Lucas <andrelucca99@gmail.com>
2023-10-03 17:12:53 -0300

    feat: add diretivas do angular

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo infos para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'As diretivas são como atributos em um elemento DOM que informa o Angular para que adicione algum comportamento a este elemento. Sobre as diretivas que conheceu nesta aula, marque as afirmativas corretas. As diretivas são como atributos em um elemento DOM que informa o Angular para que adicione algum comportamento a este elemento. Sobre as diretivas que conheceu nesta aula, marque as afirmativas corretas.',
+      autoria: 'Dev',
+      modelo: 'modelo2'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
