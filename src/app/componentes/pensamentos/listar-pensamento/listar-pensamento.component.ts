53a5622a0264932bbec7f88b23a759e43de06e1c
Andre Lucas <andrelucca99@gmail.com>
2023-10-04 09:29:02 -0300

    feat: add comunicação com o backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,33 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos: Pensamento[] = [];
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo infos para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente Filho',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'As diretivas são como atributos em um elemento DOM que informa o Angular para que adicione algum comportamento a este elemento. Sobre as diretivas que conheceu nesta aula, marque as afirmativas corretas. As diretivas são como atributos em um elemento DOM que informa o Angular para que adicione algum comportamento a este elemento. Sobre as diretivas que conheceu nesta aula, marque as afirmativas corretas.',
-      autoria: 'Dev',
-      modelo: 'modelo2'
-    },
-  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
